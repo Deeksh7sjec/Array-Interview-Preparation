15066846b104a934f74f9b76cd1c6e099228bfed
Your Name <your-email@example.com>
2026-08-11 23:53:04 +0530

    Added Maximum Subarray solution

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,22 +1,14 @@
-
 public class MaximumSubarray {
 
     public static int maxSubArray(int[] nums) {
 
         int maxSum = nums[0];
+        int currentSum = nums[0];
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
 
-            int currentSum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-
-                currentSum += nums[j];
-
-                if (currentSum > maxSum) {
-                    maxSum = currentSum;
-                }
-            }
+            currentSum = Math.max(currentSum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
         return maxSum;
@@ -26,6 +18,8 @@ public class MaximumSubarray {
 
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
-        System.out.println(maxSubArray(nums));
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
     }
 }
